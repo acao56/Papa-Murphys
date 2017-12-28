11862408a10110acbdd63aac0e712bf45314ecc7
JM <Novaaaaa@users.noreply.github.com>
2017-12-28 09:55:56 -0800

    Bad Ingredients. Bad Pizza. Papa John's

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Papa-Murphys
 Papa Johns #PassThePizza Script
+### Installation
+
+Papa-Murphys requires [Node.js](http://nodejs.org/).
+
+Setup:
+
+```sh
+$ git clone https://github.com/Novaaaaa/Papa-Murphys.git
+$ cd Papa-Murphys
+$ npm install # "sudo npm install" if you're on macOS or Linux
+```
+
+Configure information inside the `config.example.json` be sure to rename it to `config.json` when you're done. More information about configuring your script can be found at google.com
+
+
+
+Run After Setup:
+
+```sh
+$ node index
+```
+
+### What does this thing cook?
+- Everything
+
+### Proxies?
+```
+ip:port
+ip:port:user:pass
+```
+### Who
+
+Readme stolen from <a href="http://petersoboyejo.com/">@dzt</a>, made better by my nonexistent motivation.
+
+## License
+
+```
+The MIT License (MIT)
+
+Copyright (c) 2017 Peter Soboyejo <http://petersoboyejo.com/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+```
---
+++ index.js
@@ -0,0 +1,120 @@
+//rip
+const request       = require('request');
+const chalk         = require('chalk');
+const config        = require('./config.json');
+const faker         = require('faker');
+const cheerio       = require('cheerio');
+const fs            = require('fs');
+let proxyList       = [];
+
+/* -- Thanks to @hunterbdm for this -- */
+function formatProxy(proxy) {
+    if (proxy && ['localhost', ''].indexOf(proxy) < 0) {
+        proxy = proxy.replace(' ', '_');
+        const proxySplit = proxy.split(':');
+        if (proxySplit.length > 3)
+            return "http://" + proxySplit[2] + ":" + proxySplit[3] + "@" + proxySplit[0] + ":" + proxySplit[1];
+        else
+            return "http://" + proxySplit[0] + ":" + proxySplit[1];
+    }
+    else
+        return undefined;
+}
+/* -- Thanks to @hunterbdm for this -- */
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+
+
+function boot(){
+  /* -- This too -- */
+  const proxyInput = fs.readFileSync('proxies.txt').toString().split('\n');
+      for (let p = 0; p < proxyInput.length; p++) {
+        proxyInput[p] = proxyInput[p].replace('\r', '').replace('\n', '');
+        if (proxyInput[p] != '')
+            proxyList.push(proxyInput[p]);
+    }
+    console.log('Found ' + proxyList.length + ' proxies.');
+    //flex
+    console.log(chalk.green('Papa Johns Pizza Script ---- Twitter.com/incorrectCVV'));
+    console.log(chalk.green('------------------------------------------------------'))
+    for (var i = 0; i < config.Codes; i++) {
+      get()
+    }
+}
+
+function get() {
+    let session = request.jar()
+    let opts    = {
+        url     : config.Homepage,
+        method  : 'get',
+        gzip    : true,
+        jar     : session,
+        headers : {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+        },
+        proxy   : formatProxy(proxyList[Math.floor(Math.random() * proxyList.length)])
+    }
+
+
+    request(opts, function(error, response, body) {
+        if(error) {
+            console.log(chalk.red('Errror' + error))
+        } else if(response.statusCode == 200) {
+            console.log('Connected to homepage, scraping form action.')
+            let $ = cheerio.load(body);
+            //There's def a better way to scrape this but I cba so lmao
+            let action = ($('body > div > section.centered.banner.banner--dark.banner--passthepizza.paragraph-text.spacing-bottom-med > div > div > form').attr('action'));
+            if (!action) {
+              console.log('Page not available, try again later.')
+              process.exit();
+            } else if (action.length > 0) {
+              return post(session,action)
+            } else {
+              console.log('Page not available, try again later.')
+              process.exit();
+            }
+
+        } else {
+            console.log(chalk.red('unexpected error'))
+        }
+    })
+}
+function getEmail() {
+  //Need to add Gmail support
+  return faker.name.firstName()  + getRandomArbitrary(1298,199999) + config.Domain;
+}
+function post(session,action) {
+    let fname = getEmail();
+    let payload = {
+      'email': fname
+    }
+    console.log('Submitting with ' + fname);
+    let opts = {
+        url     : action,
+        method  : 'post',
+        gzip    : true,
+        jar     : session,
+        headers : {
+          "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+          "Accept-Encoding" :   "gzip, deflate",
+          "Accept-Language" :   "en-US,en;q=0.9",
+          "Cache-Control"   :   "no-cache",
+          "Connection"      :   "keep-alive",
+          "Content-Type"    :   "application/x-www-form-urlencoded"
+        },
+        proxy   : formatProxy(proxyList[Math.floor(Math.random() * proxyList.length)]),
+        form    : payload
+    };
+    request(opts, function(error, response, body) {
+        if (error) {
+            console.log('Form submission failed: ' + error)
+        } else if(response.statusCode == 200 && body.indexOf('Congrats on getting a free pizza!') > -1) {
+          console.log(chalk.green('Pizza Passed! Check your email for the code.'))
+        } else {
+            console.log('Form submission failed: ' + response.stausCode)
+        }
+    })
+}
+boot()
